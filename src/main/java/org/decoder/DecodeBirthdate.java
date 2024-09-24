93efd85cae0d3493f0cb7c22b403b8d9d8c373a0
p.mikov <p.mikov@tinqin.com>
2024-09-24 16:14:20 +0300

    Initial commit

---
+++ src/main/java/org/decoder/DecodeBirthdate.java
@@ -0,0 +1,39 @@
+package org.decoder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DecodeBirthdate {
+
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> symbols = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        String str = sc.nextLine();
+        if (str.length() == 8) {
+            String[] arrOfStr = str.split("");
+            symbols.addAll(Arrays.asList(arrOfStr));
+            for (int i = 0; i < symbols.size(); i++) {
+                switch (symbols.get(i)) {
+                    case "D" -> symbols.set(i, "0");
+                    case "R" -> symbols.set(i, "1");
+                    case "S" -> symbols.set(i, "2");
+                    case "P" -> symbols.set(i, "3");
+                    case "3" -> symbols.set(i, "4");
+                    case "Y" -> symbols.set(i, "5");
+                    case "X" -> symbols.set(i, "6");
+                    case "L" -> symbols.set(i, "7");
+                    case "B" -> symbols.set(i, "8");
+                    case "I" -> symbols.set(i, "9");
+                    default -> System.out.print(" Invalid->");
+                }
+                System.out.print(symbols.get(i));
+            }
+        } else {
+            System.out.println("Number of symbols not eight");
+        }
+    }
+
+}
---
+++ src/main/java/org/decoder/EncodeBirthdate.java
@@ -0,0 +1,39 @@
+package org.decoder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EncodeBirthdate {
+
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> symbols = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        String str = sc.nextLine();
+        if (str.length() == 8) {
+            String[] arrOfStr = str.split("");
+            symbols.addAll(Arrays.asList(arrOfStr));
+            for (int i = 0; i < symbols.size(); i++) {
+                switch (symbols.get(i)) {
+                    case "0" -> symbols.set(i, "D");
+                    case "1" -> symbols.set(i, "R");
+                    case "2" -> symbols.set(i, "S");
+                    case "3" -> symbols.set(i, "P");
+                    case "4" -> symbols.set(i, "3");
+                    case "5" -> symbols.set(i, "Y");
+                    case "6" -> symbols.set(i, "X");
+                    case "7" -> symbols.set(i, "L");
+                    case "8" -> symbols.set(i, "B");
+                    case "9" -> symbols.set(i, "I");
+                    default -> System.out.print(" Invalid->");
+                }
+                System.out.print(symbols.get(i));
+            }
+        } else {
+            System.out.println("Number of symbols not eight");
+        }
+    }
+
+}
